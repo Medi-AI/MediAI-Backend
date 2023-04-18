3d84dbdd736c9d74c1bb04e042fb77491f8bba85
Parth Sali <parthsali04@gmail.com>
2023-04-18 18:33:30 +0530

    update

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const {LoginController, RegisterController} = require("../controllers");
 
+router.get("/", (req, res) => {
+	res.status(200).json({msg: "Home Page"});
+});
 router.post("/login", LoginController.login);
 router.post("/register", RegisterController.register);
 
